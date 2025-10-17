579171c173e0ccd21b10362d5d8f5289c2e82321
yu <a961018@apps.ntpc.edu.tw>
2025-10-17 09:23:33 +0800

    guess.html,guess.js,style.css,test.html,test.js

---
+++ guess.js
@@ -0,0 +1,40 @@
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+const result = document.querySelector(".result");
+const count = document.querySelector(".count");
+
+
+let countNum =0;   //廣域變數
+let randomNumber = Math.random();
+console.log("觀察隨機的數字：", randomNumber);
+
+
+function checkGuess() {
+    countNum++;
+    count.textContent = "猜測次數："+countNum;
+    const userGuess = Number(guessField.value);  //取得欄位值，並轉為數字
+    const guesses = document.querySelector(".guesses");
+
+guesses.textContent += userGuess + " ";  
+
+
+if  (  userGuess === randomNumber ) {
+    result.textContent = "猜測結果：Congratulations!" ;
+}
+else if ( userGuess < randomNumber ) {
+    result.textContent = "猜測結果：數字太小!" ;
+}
+else if ( userGuess >  randomNumber ) {
+    result.textContent = "猜測結果：數字太大!";
+}
+
+result.textContent += "遊戲結束";
+result.style.backgroundColor="red";
+alert("遊戲結束");
+setGameOver();
+
+function setGameOver() {
+        guessField.disabled = true; //停止輸入功能
+        guessSubmit.disabled = true;    //停止按鈕功能
+
+
